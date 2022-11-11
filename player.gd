96a490793ee54cd3e9db410cd0a987f53a94e213
Julian Arnold <julian.arnold@student.hpi.de>
2022-11-11 22:13:12 +0100

    added player sprite

--- player.gd
+++ player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 
 # Declare member variables here. Examples:
 # var a = 2
-var speed = 15 #4.2 * 6.9
+var speed = 420 #15 #4.2 * 6.9
 var movementInput = Vector2(0, 0)
 
 # Called when the node enters the scene tree for the first time.
@@ -43,4 +43,4 @@ func attack():
 	$WeaponAttachment.rotation = 0
 		
 func _physics_process(delta):
-	move_and_collide(movementInput * speed)
+	move_and_slide(movementInput * speed)
