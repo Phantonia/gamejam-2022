27208c6b313d482e1f392b41be7659d5db0f1aec
Antonia <62192270+Phantonia@users.noreply.github.com>
2022-11-12 18:31:51 +0100

    Placed a lot more nazis + amount of nazis scales with level (we don't have level progression yet but pssst)

--- game.gd
+++ game.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
+var mission : int
+var dream : bool
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	GlobalVariables.level += 1
+	
+	mission = randi() % 2 + 1
+	dream = randi() % 2 == 1
+	
+	var removeCount = $nazis.get_child_count() - nazi_count_for_level(GlobalVariables.level)
+	print("removeCount = " + str(removeCount))
+	for i in range(removeCount):
+		var index = randi() % $nazis.get_child_count()
+		$nazis.get_child(index).queue_free()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func nazi_count_for_level(level):
+	var minCount = 4
+	var slope = 2
+	return min($nazis.get_child_count(), minCount + slope * level)
---
+++ global_variables.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var level = 8
+const MISSION_KILL_ALL_NAZIS = 1
+const MISSION_COLLECT_ITEM = 2
+
+func _ready():
+	pass
