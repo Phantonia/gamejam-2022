507c3a03096b6c7709b2f1ca661ba14084148903
Antonia <62192270+Phantonia@users.noreply.github.com>
2022-11-12 12:46:18 +0100

    Nazis fire bullets that can hurt the PC

--- nazi.gd
+++ nazi.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 
 var hp = 3
-
+var shoots = false
+var player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,3 +18,38 @@ func get_hit():
 	
 func die():
 	get_parent().remove_child(self)
+	
+func start_shooting(player):
+	shoots = true
+	self.player = player
+	$Timer.start()
+
+func stop_shooting():
+	shoots = false
+	player = null
+	$Timer.stop()
+
+func _on_Radius_body_entered(body):
+	if body.is_in_group("Player"):
+		start_shooting(body)
+
+
+func _on_Radius_body_exited(body):
+	if body.is_in_group("Player"):
+		stop_shooting()
+
+
+func _on_Timer_timeout():
+	var bulletScene = load("res://bullet.tscn")
+	var bullet = bulletScene.instance()
+	var dir = position.direction_to(player.position)
+	var randomAngle = randf() * PI / 12 - PI / 24
+	dir = dir.rotated(randomAngle)
+	bullet.launch(position + 20 * dir, dir)
+	get_parent().add_child(bullet)
+
+
+func _on_HitBox_body_entered(body):
+	if body.is_in_group("Bullet") and body != self:
+		print("destroy bullet")
+		body.queue_free()
