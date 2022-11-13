cde9c6ef6b29caafca32aa225c2661ab6ad830d7
Antonia <62192270+Phantonia@users.noreply.github.com>
2022-11-13 15:53:46 +0100

    Recreated scene finisher

--- game.gd
+++ game.gd
@@ -22,10 +22,15 @@ func _ready():
 	print("Dream = " + str(dream))
 	
 	var removeCount = $nazis.get_child_count() - nazi_count_for_level(GlobalVariables.level)
-#	print("removeCount = " + str(removeCount))
+	print("Remove " + str(removeCount) + " nazis to get to " + str(nazi_count_for_level(GlobalVariables.level)))
 	for i in range(removeCount):
-		var index = randi() % $nazis.get_child_count()
-		$nazis.get_child(index).queue_free()
+		while true:
+			var index = randi() % $nazis.get_child_count()
+			var child = $nazis.get_child(index)
+			if child.is_in_group("Nazi"):
+				child.queue_free()
+				$nazis.remove_child(child)
+				break
 		
 	$"CanvasLayer/nazisLeft".text = str(get_nazi_count()) + " Nazis left"
 
