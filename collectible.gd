c794913cb0d5090311159bb8ac45b0c1e56539eb
Antonia <62192270+Phantonia@users.noreply.github.com>
2022-11-13 12:04:15 +0100

    PC can now pick up collectible

---
+++ collectible.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+signal picked_up(collectible);
+
+var player = null
+
+func _ready():
+	pass
+
+
+
+func _on_Player_pick_up():
+	if player != null:
+		emit_signal("picked_up", self)
+
+
+func _on_collectible_body_entered(body):
+	if body.is_in_group("Player"):
+		player = body
+
+func _on_collectible_body_exited(body):
+	if body.is_in_group("Player"):
+		player = null
